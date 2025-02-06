6fc20711dfa6bb952202a7661f108c8ce669267c
UnLakhs <apostolos052@gmail.com>
2025-02-06 12:43:11 +0200

    removed the link to signup and replaced it with a component

--- app/Dashboard/Profile/page.tsx
+++ app/Dashboard/Profile/page.tsx
@@ -7,6 +7,7 @@ import { redirect } from "next/navigation";
 import ProfileInfoForm from "@/app/Components/ProfileInfoForm";
 import Link from "next/link";
 import { User } from "@/app/Cosntants/constants";
+import CreateUserAccount from "@/app/Components/CreateUserAccount";
 
 //Server-side function to get user data from session
 const getUserFromSession = async () => {
@@ -14,7 +15,7 @@ const getUserFromSession = async () => {
     const res = await fetch(`http://localhost:3000/api/Authentication/me`, {
       method: "GET",
       headers: {
-        Cookie: `token=${(await cookies()).get("token")?.value || ""}`,
+        Cookie: `token=${(await cookies()).get("token")?.value || ""}`, //send the cookies manually
       },
       cache: "no-store",
     });
@@ -43,17 +44,12 @@ const Profile = async () => {
   return (
     <div className="bg-blue-200 h-screen">
       <Nav />
-      <div className="flex justify-between items-start ml-60">
+      <div className="flex gap-56 items-start ml-60">
         <ProfileInfoForm />
 
         {/* This can only be seen by users with the admin role. */}
         {user?.role === "admin" && (
-          <Link
-            href={"/Authentication/SignUp"}
-            className="bg-blue-500 p-4 rounded-md text-white text-center m-auto hover:bg-blue-600 transition duration-200"
-          >
-            Make an account for a user
-          </Link>
+          <CreateUserAccount />
         )}
       </div>
     </div>
