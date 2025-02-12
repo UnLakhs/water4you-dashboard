ace81a655989d699553e42ce55ed9830b96b9bcc
UnLakhs <apostolos052@gmail.com>
2025-02-12 13:02:05 +0200

    Added pooling to mongo so that it won't connect seven trillion times

--- app/lib/mongoDB.ts
+++ app/lib/mongoDB.ts
@@ -1,9 +1,30 @@
 import { MongoClient } from "mongodb";
 
-if(!process.env.MONGODB_URI)
-    throw new Error("Please add Mongo URI to .env");
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add Mongo URI to .env");
+}
 
-const client = new MongoClient(process.env.MONGODB_URI);
-const clientPromise: Promise<MongoClient> = client.connect();
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+const uri = process.env.MONGODB_URI;
+const options = {maxPoolSize: 10};  //Connection pooling to reuse coonnections
+
+let client;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  // Use a global variable in development to prevent multiple connections
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  // In production, only create a single instance
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
 
 export default clientPromise;
