8d747deef112cfe460866a9aedf118512170272c
UnLakhs <apostolos052@gmail.com>
2025-02-24 11:34:26 +0200

    changed some .env stuff

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -13,11 +13,11 @@ export async function GET() {
       process.env.TWILIO_AUTH_TOKEN
     );
 
-    //nodemailer transporter
+    // Initialize Nodemailer transporter.
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: parseInt(process.env.EMAIL_PORT || "587"),
-      secure: process.env.EMAIL_SECURE === "true", // true for 465, false for other ports.
+      secure: process.env.EMAIL_SECURE === "true", // true for port 465, false for 587
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
